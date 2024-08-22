9ca719b80add85e63e5bd357e3ebd784bb31eb38
deepak9285 <83562221+deepak9285@users.noreply.github.com>
2024-08-22 19:15:09 +0530

    null

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const {signup, login}=require("../controllers/auth");
-const { acessChat, fetchChats } = require("../controllers/chat");
+
 router.post("/signup",signup);
 router.post("/login",login);
 
